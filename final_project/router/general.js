755c052fee4de76155b740ffe48ca597b8f68480
HRMeister <hrmeister@gmail.com>
2024-11-21 15:35:04 -0500

    All tasks donde

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios').default;
 
 // Check if a user with the given username already exists
 const doesExist = (username) => {
@@ -38,53 +39,81 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+public_users.get('/',async (req, res) => {
+    try{
+        const bookList = await axios.get(books)
+        res.send(JSON.stringify(bookList,null,4));
+    }
+    catch(error){
+        res.status(500).send({
+            error: "Could not get book list"
+        });
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async (req, res) => {
     const ISBN = req.params.isbn;
-    res.send(books[ISBN]);
+
+    try {
+        const book = await axios.get(books[ISBN])
+        if(book){
+            res.send(book);
+        }
+    }
+    catch(error){
+        res.status(500).send({
+            error: "No book with that ISBN"
+        });
+    }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async (req, res) => {
    const author = req.params.author;
-   const bookKeys = Object.keys(books);
-
-    const matchBooks = [];
-    for(const key of bookKeys){
-        if(books[key].author === author){
-            matchBooks.push(books[key]);
+   try{
+        const bookList = await axios.get(books)
+        const bookKeys = Object.keys(bookList);
+        const matchBooks = [];
+        for(const key of bookKeys){
+            if(books[key].author === author){
+                matchBooks.push(books[key]);
+            }
         }
-    }
-
-    if (matchBooks.length === 0) {
-    return res.status(404).send({ message: "No books found for the given author." });
-    }
-
-   res.send(matchBooks);
-
+        if (matchBooks.length === 0) {
+        return res.status(404).send({ message: "No books found for the given author." });
+        }
+        res.send(matchBooks);
+   }
+   catch(error){
+        res.status(500).send({
+        error: "No book with that author"
+    });
+   }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async (req, res)=> {
     const title = req.params.title;
-    const bookKeys = Object.keys(books);
-
-    const matchBooks = [];
-    for(const key of bookKeys){
-        if(books[key].title === title){
-            matchBooks.push(books[key]);
+    try{
+        const bookList = await axios.get(books)
+        const bookKeys = Object.keys(bookList);
+        const matchBooks = [];
+        for(const key of bookKeys){
+            if(books[key].title === title){
+                matchBooks.push(books[key]);
+            }
         }
+        if (matchBooks.length === 0) {
+        return res.status(404).send({ message: "No books found for the given author." });
+        }
+    res.send(matchBooks);
     }
-
-    if (matchBooks.length === 0) {
-    return res.status(404).send({ message: "No books found for the given author." });
-    }
-
-   res.send(matchBooks);
+    catch(error){
+        res.status(500).send({
+            error: "No book with that title"
+    });
+    }   
 });
 
 //  Get book review
